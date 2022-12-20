365570d33edf0c6c2bb091b9c336b52017c681bc
Aiden Scott <aiden@oldstreetjournal.co.uk>
2022-12-20 22:03:02 +0000

    add nixosModule bit

--- flake.nix
+++ flake.nix
@@ -7,36 +7,44 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
+      };
       overlay = final: prev: {
         dwm = prev.dwm.overrideAttrs (old: {
           version = "6.4";
           src = builtins.path { path = ./.; name = "dwm"; };
         });
       };
-
-      dwm = (
-        import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        }
-      ).dwm;
     in
     {
       overlays.default = overlay;
+      packages.${system}.default = pkgs.dwm;
 
-      packages.${system}.default = dwm;
+      # checks.${system} = {
+      #   build = dwm;
 
-      checks.${system} = {
-        build = dwm;
-
-        version = nixpkgs.legacyPackages.${system}.runCommand "version-check" { } ''
-          dwm_version="$(${dwm}/bin/dwm -v 2>&1 || :)"
-          echo "package version: ${dwm.name}"
-          echo "dwm version:     $dwm_version"
-          [[ "${dwm.name}" == "$dwm_version" ]]
-          touch ${placeholder "out"}
-        '';
+      #   version = nixpkgs.legacyPackages.${system}.runCommand "version-check" { } ''
+      #     dwm_version="$(${dwm}/bin/dwm -v 2>&1 || :)"
+      #     echo "package version: ${dwm.name}"
+      #     echo "dwm version:     $dwm_version"
+      #     [[ "${dwm.name}" == "$dwm_version" ]]
+      #     touch ${placeholder "out"}
+      #   '';
+      # };
+      nixosModules.default = {
+        config = {
+          services.xserver.windowManager.session = pkgs.lib.singleton {
+            name = "dwm+aiden";
+            start =
+              ''
+                startdwm &
+                waitPID=$!
+              '';
+          };
+        };
       };
     };
+
 }
